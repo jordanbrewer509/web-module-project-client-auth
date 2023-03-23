d4ba446b995a1462dd9b97a5780669ea23d4f032
Jordan~ <jordan.brewer509@gmail.com>
2023-03-23 12:17:08 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Route, Routes, Link, Redirect, useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  return (<h2>Login</h2>)
+}
+
+const FriendList = () => {
+  return (<h2>Login</h2>)
+}
+
+const AddFriend = () => {
+  return (<h2>Login</h2>)
+}
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <Routes>
+        <Route path="/" 
+          element={<Login />}>
+        </Route>
+        <Route path="/login"
+        element={<Login />}> 
+          {navigate("/")}
+        </Route>
+        <Route path="/friends" 
+          element={<FriendList />}>
+        </Route>
+        <Route path="/friends/add" 
+          element={<AddFriend />}>
+        </Route>
+      </Routes>
     </div>
   );
 }
